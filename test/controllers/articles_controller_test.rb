3763fe98298b7f174053dac05cafe1182f47434a
DEugeneS <davydes@gmail.com>
2014-10-16 17:51:27 +0400

    fix basic tests

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -1,8 +1,11 @@
 require 'test_helper'
 
 class ArticlesControllerTest < ActionController::TestCase
+  include SessionsHelper
+
   setup do
     @article = articles(:one)
+    @user = users(:one)
   end
 
   test "should get index" do
@@ -12,16 +15,23 @@ class ArticlesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    user = User.find_by_email(@user.email)
+    sign_in(user)
     get :new
     assert_response :success
+    sign_out
   end
 
   test "should create article" do
+    user = User.find_by_email(@user.email)
+    sign_in(user)
+
     assert_difference('Article.count') do
-      post :create, article: { content: @article.content, title: @article.title, user_id: @article.user_id }
+      post :create, article: { content: @article.content, title: @article.title }
     end
 
     assert_redirected_to article_path(assigns(:article))
+    sign_out
   end
 
   test "should show article" do
@@ -30,20 +40,31 @@ class ArticlesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    user = User.find_by_email(@user.email)
+    sign_in(user)
     get :edit, id: @article
     assert_response :success
+    sign_out
   end
 
   test "should update article" do
-    patch :update, id: @article, article: { content: @article.content, title: @article.title, user_id: @article.user_id }
+    user = User.find_by_email(@user.email)
+    sign_in(user)
+    patch :update, id: @article, article: { content: @article.content, title: @article.title }
     assert_redirected_to article_path(assigns(:article))
+    sign_out
   end
 
   test "should destroy article" do
+    user = User.find_by_email(@user.email)
+    sign_in(user)
+
     assert_difference('Article.count', -1) do
       delete :destroy, id: @article
     end
 
     assert_redirected_to articles_path
+    sign_out
   end
+
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,14 +1,19 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
+  include SessionsHelper
+
   setup do
     @user = users(:one)
   end
 
   test "should get index" do
+    user = User.find_by_email(@user.email)
+    sign_in(user)
     get :index
     assert_response :success
     assert_not_nil assigns(:users)
+    sign_out
   end
 
   test "should get new" do
@@ -18,7 +23,7 @@ class UsersControllerTest < ActionController::TestCase
 
   test "should create user" do
     assert_difference('User.count') do
-      post :create, user: { email: @user.email, name: @user.name }
+      post :create, user: { email: 'test_create@mail.org', name: 'create_test', password: 'password', password_confirmation: 'password' }
     end
 
     assert_redirected_to user_path(assigns(:user))
@@ -30,18 +35,35 @@ class UsersControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    user = User.find_by_email(@user.email)
+    sign_in(user)
     get :edit, id: @user
+    sign_out
     assert_response :success
   end
 
-  test "should update user" do
-    patch :update, id: @user, user: { email: @user.email, name: @user.name }
+  test "should update user with password" do
+    user = User.find_by_email(@user.email)
+    sign_in(user)
+    patch :update, id: @user, user: { email: @user.email, name: @user.name, password: 'qwerty', password_confirmation: 'qwerty' }
     assert_redirected_to user_path(assigns(:user))
+    sign_out
+  end
+
+  test "should not update user different password and confirmation" do
+    user = User.find_by_email(@user.email)
+    sign_in(user)
+    patch :update, id: @user, user: { email: @user.email, name: @user.name, password: 'qwerty', password_confirmation: 'qwertyu' }
+    assert_response :success
+    sign_out
   end
 
   test "should destroy user" do
     assert_difference('User.count', -1) do
+      user = User.find_by_email(@user.email)
+      sign_in(user)
       delete :destroy, id: @user
+      sign_out
     end
 
     assert_redirected_to users_path
