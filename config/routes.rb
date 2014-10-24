e094e20d4247db26a16ffb899a0e0453f067c8b2
DEugeneS <davydes@gmail.com>
2014-10-25 02:54:06 +0400

    revert routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
     get :switch_format, on: :collection
   end
 
-  #get '/signup', to: 'users#new'
+  get '/signup', to: 'users#new'
   get '/signin', to: 'sessions#new'
   get '/signout', to: 'sessions#destroy'
 
