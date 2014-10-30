78343dfeef05b41d3b579a16ab2c4971dea1a572
DEugeneS <davydes@gmail.com>
2014-10-31 00:26:48 +0300

    next change albums && arrange locales

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -15,7 +15,7 @@ class AlbumsController < ApplicationController
 
   # GET /albums/new
   def new
-    @album = Album.new()
+    @album = current_user.albums.new()
   end
 
   # GET /albums/1/edit
@@ -28,8 +28,8 @@ class AlbumsController < ApplicationController
     @album = current_user.albums.new(album_params)
     respond_to do |format|
       if @album.save
-        format.html { redirect_to @album, notice: 'Album was successfully created.' }
-        format.json { render :show, status: :created, location: @album }
+        format.html { redirect_to [@album.user, @album], notice: 'Album was successfully created.' }
+        format.json { render :show, status: :created, location: [@album.user, @album] }
       else
         format.html { render :new }
         format.json { render json: @album.errors, status: :unprocessable_entity }
@@ -42,8 +42,8 @@ class AlbumsController < ApplicationController
   def update
     respond_to do |format|
       if @album.update(album_params)
-        format.html { redirect_to @album, notice: 'Album was successfully updated.' }
-        format.json { render :show, status: :ok, location: @album }
+        format.html { redirect_to [@album.user, @album], notice: 'Album was successfully updated.' }
+        format.json { render :show, status: :ok, location: [@album.user, @album] }
       else
         format.html { render :edit }
         format.json { render json: @album.errors, status: :unprocessable_entity }
@@ -56,7 +56,7 @@ class AlbumsController < ApplicationController
   def destroy
     @album.destroy
     respond_to do |format|
-      format.html { redirect_to albums_url, notice: 'Album was successfully destroyed.' }
+      format.html { redirect_to user_albums_path(@album.user), notice: 'Album was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
