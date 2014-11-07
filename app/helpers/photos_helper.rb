880f95aadebd984ec52caf6cfc5985243d6385a6
DEugeneS <davydes@gmail.com>
2014-11-08 02:47:29 +0300

    improve views

--- app/helpers/photos_helper.rb
+++ app/helpers/photos_helper.rb
@@ -2,4 +2,8 @@ module PhotosHelper
   def image_photo_url(photo, style = :medium)
     url_to_attachment(photo.image, style)
   end
+
+  def photo_id (album)
+    "user_#{album.user.id}_photo_#{album.id}"
+  end
 end
