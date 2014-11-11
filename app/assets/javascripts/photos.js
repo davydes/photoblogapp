ccd7c663b4458c0e131970408754ce3ec2a23b11
DEugeneS <davydes@gmail.com>
2014-11-12 00:30:26 +0300

    save last changes

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,10 +1,1 @@
-var setMaxPhotoHeight = function() {
-    $('#main-photo').setHeightAsWindow(85);
-};
-
-$(document).ready(function () {
-    $('body').setFadeAble('div.context-item', 'a.control');
-    setMaxPhotoHeight();
-    $(window).resize(setMaxPhotoHeight);
-});
 
