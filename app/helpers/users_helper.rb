f08c35f41c2d53319bb1e31cf19b4b4ad65fda3e
DEugeneS <davydes@gmail.com>
2014-11-14 00:03:54 +0300

    _user_card updated

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def image_avatar_url(user, style = :thumb)
     url_to_attachment(user.avatar, style)
   end
+
+  def masked_user_email(user)
+    admin_or_current?(user) ? mail_to(user.email) : user.email.gsub(/.{1,}@/, '***@')
+  end
 end
