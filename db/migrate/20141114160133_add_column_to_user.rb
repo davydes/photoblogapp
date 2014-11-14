3c8fb660d4dc937d83e19c10ae79184dd6ffc1df
DEugeneS <davydes@gmail.com>
2014-11-14 19:06:02 +0300

    users migration, add new columns

---
+++ db/migrate/20141114160133_add_column_to_user.rb
@@ -0,0 +1,10 @@
+class AddColumnToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :first_name, :string, :null => false
+    add_column :users, :last_name, :string, :null => false
+    add_column :users, :gender, :string, :limit => 1
+    add_column :users, :country, :string
+    add_column :users, :city, :string
+    add_column :users, :date_of_birth, :date
+  end
+end
