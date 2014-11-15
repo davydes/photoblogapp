28851117b04a89b248b77caefc9cec02e5100566
DEugeneS <davydes@gmail.com>
2014-11-16 00:19:19 +0300

    I18n.default_locale => :ru

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module PhotoBlogApp
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.default_locale = :ru
 
     # Override action_view proc
     config.action_view.field_error_proc = Proc.new { |html_tag, instance|
