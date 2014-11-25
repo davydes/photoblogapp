81f4f09d0f6e40f735f16a3fd089c8315a15686a
DEugeneS <davydes@gmail.com>
2014-11-25 18:34:44 +0300

    add jumpToHash into application JS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -22,34 +22,42 @@
 //= require_directory .
 
 (function($) {
-    $.fn.renderFormErrors = function(errors){
-        this.clearPreviousErrors();
-        var model = this.data('model');
+  $.fn.renderFormErrors = function(errors){
+    this.clearPreviousErrors();
+    var model = this.data('model');
 
-        // show error messages in input form-group help-block
-        $.each(errors, function(field, messages){
-            if (messages.length > 0) {
-                var input = $('#'+model+'_'+field);
-                input.closest('.form-group').addClass('has-error').find('.help-block').html(messages.join(' & '));
-            }
-        });
-    };
+    // show error messages in input form-group help-block
+    $.each(errors, function(field, messages){
+      if (messages.length > 0) {
+          var input = $('#'+model+'_'+field);
+          input.closest('.form-group').addClass('has-error').find('.help-block').html(messages.join(' & '));
+      }
+  });
+  };
 
-    $.fn.clearPreviousErrors = function(){
-        $('.form-group.has-error', this).each(function(){
-            $('.help-block', $(this)).html('');
-            $(this).removeClass('has-error');
-        });
-    };
+  $.fn.clearPreviousErrors = function(){
+    $('.form-group.has-error', this).each(function(){
+      $('.help-block', $(this)).html('');
+      $(this).removeClass('has-error');
+    });
+  };
 
-    $.fn.setFadeAble = function(baseElementSelector, affectElementSelector, maxOpacity) {
-        maxOpacity = typeof maxOpacity !== 'undefined' ? maxOpacity : 1;
-        $(this).find(baseElementSelector).on('mouseenter', function () {
-            $(this).find(affectElementSelector).fadeTo('faset', maxOpacity);
-        });
-        $(this).find(baseElementSelector).on('mouseleave', function(){
-            $(this).find(affectElementSelector).stop().fadeOut();
-        });
-    };
+  $.fn.setFadeAble = function(baseElementSelector, affectElementSelector, maxOpacity) {
+    maxOpacity = typeof maxOpacity !== 'undefined' ? maxOpacity : 1;
+    $(this).find(baseElementSelector).on('mouseenter', function () {
+      $(this).find(affectElementSelector).fadeTo('faset', maxOpacity);
+    });
+    $(this).find(baseElementSelector).on('mouseleave', function(){
+      $(this).find(affectElementSelector).stop().fadeOut();
+    });
+  };
 
+  $.fn.jumpToHash = function(hash, offsetY) {
+    var el = $(this).find(hash);
+    if ( el.length ) {
+      $(this).scrollTop(el.offset().top-offsetY);
+      return true;
+    }
+    return false;
+  };
 }(jQuery));
--- app/assets/javascripts/articles.js
+++ app/assets/javascripts/articles.js
@@ -1,11 +1,11 @@
 (function($) {
-    $(function () {
-        $('.article-preview .jcarousel').jcarousel({
-            wrap: 'circular'
-        }).jcarouselAutoscroll({
-            interval: 3000,
-            target: '+=1',
-            autostart: true
-        });
+  $(function () {
+    $('.article-preview .jcarousel').jcarousel({
+      wrap: 'circular'
+    }).jcarouselAutoscroll({
+      interval: 3000,
+      target: '+=1',
+      autostart: true
     });
+  });
 }(jQuery));
