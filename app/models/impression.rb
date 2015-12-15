ee1a1c06b0f9cb62d45d02d504026fab3bb508db
davydes <davydes@gmail.com>
2015-12-15 14:33:15 +0300

    avoid save impressions without referer

--- app/models/impression.rb
+++ app/models/impression.rb
@@ -1,3 +1,15 @@
 class Impression < ActiveRecord::Base
   belongs_to :impressionable, :polymorphic=>true
+
+  before_save :check_for_referer
+
+  private
+
+  def check_for_referer
+    if referer.present?
+      return true
+    else
+      return false
+    end
+  end
 end
